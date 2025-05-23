7bd99ed9d18e45ea8fcd7b211925ea97f8a69a7e
zedsousa <hyuji125@gmail.com>
2025-05-23 00:50:00 +0000

    NM-6: Implementar Swagger
    Closes #6

--- news/serializers.py
+++ news/serializers.py
@@ -5,6 +5,7 @@ from .models import News
 
 class NewsSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(read_only=True)
+    published_date = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = News
--- news_manager/settings.py
+++ news_manager/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "news",
     "rest_framework",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "News Manager API",
+    "DESCRIPTION": "API for news management",
+    "VERSION": "1.0.0",
+}
