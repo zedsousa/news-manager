6cfe18dcf5a7710387c57ec04446d841e9fabb7a
José de Sousa <zedsousa.dev@gmail.com>
2025-05-25 23:29:06 +0000

    NM-22: Implementar filtro de notícias para cada tipo de usuário

--- news/views.py
+++ news/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import permissions, viewsets
 
 from .models import News
@@ -5,9 +6,42 @@ from .serializers import NewsSerializer
 
 
 class NewsViewSet(viewsets.ModelViewSet):
-    queryset = News.objects.all()
     serializer_class = NewsSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role == "admin":
+            return News.objects.all()
+
+        if user.role == "editor":
+            return News.objects.filter(
+                Q(author=user)
+                | (
+                    Q(status="published")
+                    & (
+                        Q(is_pro=False)
+                        | (
+                            Q(is_pro=True)
+                            & Q(vertical__in=user.plan.verticals.all())
+                            & Q(author__plan__is_pro=True)
+                        )
+                    )
+                )
+            ).distinct()
+
+        if user.role == "reader":
+            return News.objects.filter(
+                Q(status="published")
+                & (
+                    Q(is_pro=False)
+                    | (
+                        Q(is_pro=True)
+                        & Q(vertical__in=user.plan.verticals.all())
+                        & Q(author__plan__is_pro=True)
+                    )
+                )
+            ).distinct()
+
+        return News.objects.none()
